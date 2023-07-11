0c5fb5b9bf14d1ef657bcd4bebccd771f685cb13
object-ions <moshikoatia@gmail.com>
2023-07-11 13:44:59 -0700

    add logic to the app. nvp is resolved.
    Co-Authored-By: Natalie Benjes natalie.benjes@gmail.com

--- SmallChange/Models/SmallChangeClass.cs
+++ SmallChange/Models/SmallChangeClass.cs
@@ -10,24 +10,24 @@ namespace SmallChange.Models
         // Business Logic goes here.
 
         public static List<int> Break(int userInputNumber)
+    {
+        List<int> changeValues = new List<int>(new int[4]);
+
+        for (int i = 0; i <= 3; i++)
         {
-            List<int> changeValues = new List<int>(new int[4]);
+            // Save a whole number into the result
+            int coinsAmount = userInputNumber / _changeKeys[i];
+            userInputNumber %= _changeKeys[i];
 
-            for (int i = 0; i <= 3; i++)
-            {
-                // Save a whole number into the result
-                int coinsAmount = userInputNumber / _changeKeys[i];
-                userInputNumber %= _changeKeys[i];
-
-                // Set coins to the list
-                changeValues[i] = coinsAmount;
-            }
-
-            // Show result
-            Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-            Console.WriteLine($"Your Change is: {changeValues[0]} quarters, {changeValues[1]} dimes, {changeValues[2]} nickles, {changeValues[3]} pennies");
-
-            return changeValues;
+            // Set coins to the list
+            changeValues[i] = coinsAmount;
         }
+
+        // Show result
+        Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+        Console.WriteLine($"Your Change is: {changeValues[0]} quarters, {changeValues[1]} dimes, {changeValues[2]} nickles, {changeValues[3]} pennies");
+
+        return changeValues;
+    }
     }
 }
