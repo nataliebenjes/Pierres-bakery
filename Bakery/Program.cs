da03195c65efaca9c14428388a98c93bc5106a3f
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-14 10:45:10 -0700

    update README

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -10,6 +10,7 @@ class Program
   {
     Console.WriteLine("Hello, welcome to Pierre's Bakery!");
 Console.WriteLine("Bread is Buy 2, get 1 free. A single loaf costs $5");
+Console.WriteLine("If you would like a pastry, the deal is if you Buy 3, get 1 free. A single pastry costs $2, then");
     string hello = null;
     hello.Split("n");
   }
