30a858d337ed749d80e005e355a8379292cd68da
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-14 16:38:49 -0700

    add Pastry class CreateList test

--- Bakery.Tests/ModelTests/PastryClassTests.cs
+++ Bakery.Tests/ModelTests/PastryClassTests.cs
@@ -3,6 +3,19 @@ using Bakery.Models;
 using System.Collections.Generic;
 
 namespace Bakery.Tests;
-// {
-//   [TestClass]
-// }
+
+[TestClass]
+public class PastryTest
+{
+  [TestMethod]
+  public void CreateListUpToInput()
+  {
+    // Arrange
+    List<int> expectedList = new List<int>() { 0, 1, 2, 3, 4, 5, 6 };
+    List<int> resultList = Pastry.ListOf(6);
+
+    // Act & Assert
+    CollectionAssert.AreEqual(expectedList, resultList);
+  }
+
+}
