548b8407c8fcbfc05b3e1b0007d5ab059285d5be
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-14 11:13:02 -0700

    add list to bread class

--- Bakery/Models/Bread.cs
+++ Bakery/Models/Bread.cs
@@ -1,11 +1,36 @@
 using System.Collections.Generic;
 using System;
+using Bakery.Models;
 
-namespace SmallChange.Models
+//one loaf is $5
+//two loaves costs $10
+// three loaves cost $10
+// four loaves costs $15
+// five loaves cost $20
+// six loaves costs $20
+// seven loaves cost $25
+// Can you see a pattern? Every 3rd loaf of bread is free.
+//write loop, every time i % 3, add 0 to cost
+namespace Bakery.Models
 {
-    public class Bread
+  public class Bread
+  {
+    public int breadInputNumber { get; set; }
+    //need to create object to loop through up to bread input number
+    List<int> numberList = new List<int>();
+
+for (int i = 1; i <= bread; i++)
+{
+    numberList.Add(i);
+}
+    for (int i = 0; i<length; i++)
+{
+    if (i % 3 == 0)
     {
-        
-        }
+        // Index value is divisible by 3
+        // Do something here
+    }
+}
+}
     }
 
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,10 +9,17 @@ class Program
   static void Main()
   {
     Console.WriteLine("Hello, welcome to Pierre's Bakery!");
-Console.WriteLine("Bread is Buy 2, get 1 free. A single loaf costs $5");
-Console.WriteLine("If you would like a pastry, the deal is if you Buy 3, get 1 free. A single pastry costs $2, then");
-    string hello = null;
-    hello.Split("n");
+    Console.WriteLine("Bread is Buy 2, get 1 free. A single loaf costs $5");
+    Console.WriteLine("If you would like a pastry, the deal is if you Buy 3, get 1 free. A single pastry costs $2, then");
+    Console.WriteLine("How many loaves of bread would you like? Please enter a number between 0 and 10, then press enter");
+    string breadInput = Console.ReadLine();
+    int breadInputNumber = int.Parse(breadInput);
+    Console.WriteLine("How many pastries would you like? Please enter a number between 0 and 10, then press enter");
+    string pastryInput = Console.ReadLine();
+    int pastryNumber = int.Parse(pastryInput);
+    //calculate bread cost
+    //calculate pastry cost
+    //Console.Writeline("your total cost is");
   }
+
 }
-    
