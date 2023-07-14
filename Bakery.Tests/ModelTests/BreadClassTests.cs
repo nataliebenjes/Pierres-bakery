e8b5bf3cc0f8494e38653705d94cb34f3603e2a9
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-14 16:33:49 -0700

    DetermineBreadTotal test good fail

--- Bakery.Tests/ModelTests/BreadClassTests.cs
+++ Bakery.Tests/ModelTests/BreadClassTests.cs
@@ -17,12 +17,13 @@ namespace Bakery.Tests
       // Act & Assert
       CollectionAssert.AreEqual(expectedList, resultList);
     }
+    [TestMethod]
     public void DetermineBreadTotal()
     {
       List<int> potentialBread = new List<int>() { 0, 1, 2, 3, 4, 5, 6 };
       int expectedTotal = 0;
       int actualTotal = Bread.CalcTotal(potentialBread, expectedTotal);
-      Assert.AreEqual(10, actualTotal);
+      Assert.AreEqual(, actualTotal);
     }
   }
 }
